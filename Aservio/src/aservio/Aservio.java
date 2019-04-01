308022ff7b2deddefcb4e4ee39a71b641113aa32
Unknown <s.bangslund@hotmail.dk>
2019-04-01 14:55:43 +0200

    Initial code commit

---
+++ Aservio/src/aservio/Aservio.java
@@ -0,0 +1,35 @@
+/*
+ * Created by Samuel Bangslund, Odense SDU Software Engineering 1. semester.
+ */
+package aservio;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sbang
+ */
+public class Aservio extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
