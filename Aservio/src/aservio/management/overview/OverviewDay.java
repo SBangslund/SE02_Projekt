eb5808226516d22b9ec6e9ee9eabc0319fa431fe
Bende Siewertsen <bende1308@gmail.com>
2019-04-02 17:25:31 +0200

    working version of overviewday

--- Aservio/src/aservio/management/overview/OverviewDay.java
+++ Aservio/src/aservio/management/overview/OverviewDay.java
@@ -1,5 +1,8 @@
 package aservio.management.overview;
 
+
+import aservio.management.Management;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
@@ -14,16 +17,20 @@ import javafx.scene.text.Text;
 import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class OverviewDay extends Overview implements Initializable{
 
-    Date date;
+    @FXML
+    public GridPane gridPane;
+    public Label labelTid;
+    public Label labelDate;
+    public ScrollPane scrollPane;
 
+    Date date;
     Parent root;
+    ArrayList<Pane> hourPanes;
+    ArrayList<Pane> hourContentPanes;
 
     public OverviewDay() {
         this(new Date());
@@ -33,48 +40,49 @@ public class OverviewDay extends Overview implements Initializable{
         this.date = date;
         this.initialize();
     }
-
-    @Override
-    protected void initialize() {
-
-        try {
-            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("aservio/management/views/FXMLOverviewDay.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("unable to find the FXML Document");
-        }
-        // TODO create day overview
-        System.out.println("Created overview day");
-        ScrollPane root = new ScrollPane();
-        GridPane grid = new GridPane();
-        grid.setVgap(1);
-        grid.setHgap(1);
-
-        grid.add(new Label(String.format("%s, den %tD", new SimpleDateFormat("EE", new Locale("da", "DK")).format(date.getTime()), Calendar.getInstance().getTime())), 1,0);
-
-        grid.setGridLinesVisible(true);
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 23; j++) {
-                int width = i == 0 ? 50 : 300;
-                Pane pane = new Pane();
-                //pane.getChildren().add(new Text(String.format("%d, %d", i, j)));
-                pane.setPrefWidth(width);
-                pane.setPrefHeight(50);
-                grid.add(pane, i, j);
-            }
-        }
-
-
-        //root.setContent(grid);
-        super.setView(root);
-    }
-
-    public void fillGrid(){
-
-    }
+    
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        System.out.println("Created overview day");
+        labelDate.setText(String.format("%s, den %tD", new SimpleDateFormat("EEE", new Locale("da", "DK")).format(date.getTime()), Calendar.getInstance().getTime()));
+        hourPanes = new ArrayList<>();
+        hourContentPanes = new ArrayList<>();
+        gridPane.add(new Label("something"), 1, 1);
+        fillGrid(gridPane);
+        gridPane.setGridLinesVisible(true);
 
+    }
+
+    public void fillGrid(GridPane pane){
+        fillTime(pane);
+        fillTimeContent(pane, new ArrayList<String>());
+    }
+    
+    public void fillTime(GridPane pane){
+        for (int i = 1; i < 23; i++) {
+            Pane hour = new Pane();
+            pane.setMinHeight(30);
+            hour.getChildren().add(new Text(i < 10 ? "0" + Integer.toString(i) : Integer.toString(i)));
+            pane.add(hour, 0, i);
+            hourPanes.add(hour);
+        }
+    }
+
+    public void fillTimeContent(GridPane pane, ArrayList<String> activities){
+        for (int i = 1; i < 23; i++) {
+            Pane content = new Pane();
+            pane.setMinHeight(30);
+            content.getChildren().add(new Text("Activitynumber: " +  Math.random() * i));
+            pane.add(content, 1, i);
+            hourPanes.add(content);
+        }
+    }
+
+    @Override
+    protected void initialize() {
+        
     }
 }
