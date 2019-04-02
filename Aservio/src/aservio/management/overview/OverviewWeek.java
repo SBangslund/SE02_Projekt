0906a08b729d133b915c2834af03f83456c63bbc
Unknown <rehen17@student.sdu.dk>
2019-04-02 16:07:46 +0200

    Small changes

--- Aservio/src/aservio/management/overview/OverviewWeek.java
+++ Aservio/src/aservio/management/overview/OverviewWeek.java
@@ -1,42 +1,15 @@
 package aservio.management.overview;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
 
-<<<<<<< HEAD
 public class OverviewWeek extends Overview implements CalenderInterface {
 
-    private int[] days;
-    private int daysIndex;
-    
-     public OverviewWeek() {
-        this.initialize();
-    }
 
     @Override
     protected void initialize() {
-        Parent root = new Label();
-        try {
-            // TODO create week overview
-            root = FXMLLoader.load(getClass().getResource("FXMLOverviewWeek.fxml"));
-        } catch (IOException ex) {
-            System.out.println("Could not find file");
-        }
-        super.setView(root);
-=======
-    @Override
-    public void initialize() {
-        // TODO create week overview
-        System.out.println("Created overview week");
->>>>>>> feature.SePlanlægning
+     
     }
 }
