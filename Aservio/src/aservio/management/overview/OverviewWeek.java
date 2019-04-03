6cd297403fdee4f545a343641fd22bc76fd3bce8
Unknown <s.bangslund@hotmail.dk>
2019-04-03 16:39:47 +0200

    Made Overview initializable and added Calender to OverviewMonth

--- Aservio/src/aservio/management/overview/OverviewWeek.java
+++ Aservio/src/aservio/management/overview/OverviewWeek.java
@@ -2,16 +2,23 @@ package aservio.management.overview;
 
 import aservio.management.activities.ActivityList;
 
+import java.net.URL;
 import java.util.Date;
+import java.util.ResourceBundle;
 
 public class OverviewWeek extends Overview {
 
-    protected OverviewWeek() {
+    public OverviewWeek() {
         super(new Date());
     }
 
     @Override
     public void initialize() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         // TODO create week overview
         System.out.println("Created overview week");
     }
