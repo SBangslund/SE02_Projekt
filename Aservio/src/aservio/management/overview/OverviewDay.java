3125acc7f14d17c7df9942d0031ac44962d723e3
Bende Siewertsen <bende1308@gmail.com>
2019-04-04 16:59:05 +0200

    Button allocation, not really working yet

--- Aservio/src/aservio/management/overview/OverviewDay.java
+++ Aservio/src/aservio/management/overview/OverviewDay.java
@@ -8,19 +8,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.*;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+
 import java.net.URL;
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.List;
 
-public class OverviewDay extends Overview implements Initializable{
+public class OverviewDay extends Overview implements Initializable {
 
     @FXML
     public GridPane gridPane;
@@ -35,9 +34,10 @@ public class OverviewDay extends Overview implements Initializable{
     ArrayList<Pane> hourContentPanes;
 
     ActivityList activityList;
-    Map<Integer, List<Activity>> activitiesMap;
 
-    public OverviewDay(){
+    ArrayList<Button> eventButtonList;
+
+    public OverviewDay() {
         this(new Date(), new ActivityList());
     }
 
@@ -45,39 +45,39 @@ public class OverviewDay extends Overview implements Initializable{
         this(new Date(), activitiesList);
     }
 
-    public OverviewDay(Date date, ActivityList activityList){
+    public OverviewDay(Date date, ActivityList activityList) {
         super(date);
         this.date = date;
         this.activityList = activityList;
         this.initialize();
+        eventButtonList = new ArrayList<>();
     }
-    
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         scrollPane.setFitToWidth(true);
 
-        DayOfWeekLabel.setText(String.format("%s", new SimpleDateFormat("EEEEE",defineLocaleDate()).format(date.getTime())));
+        DayOfWeekLabel.setText(String.format("%s", new SimpleDateFormat("EEEEE", defineLocaleDate()).format(date.getTime())));
         moreInformationLabel.setText(String.format("den %tD", Calendar.getInstance().getTime()));
 
         hourPanes = new ArrayList<>();
         hourContentPanes = new ArrayList<>();
-        normalPane.setStyle("-fx-border-color: black");
 
         //adding test activities;
         activityList.getActivities().add(new Activity("Løbe", new Date(), standartEndDate(new Date())));
-        activityList.getActivities().add(new Activity("Meeting", new GregorianCalendar(2019, Calendar.APRIL, 2, 8, 20).getTime(), standartEndDate(new Date())));
+        activityList.getActivities().add(new Activity("Meeting", new GregorianCalendar(2019, Calendar.APRIL, 2, 12, 20).getTime(), standartEndDate(new Date())));
 
 
         fillPane(normalPane, activityList.getActivities());
 
     }
 
-    public void fillPane(Pane pane, List<Activity> activities){
+    public void fillPane(Pane pane, List<Activity> activities) {
         fillTime(pane);
         fillTimeContent(pane, activities);
     }
-    
+
     public void fillTime(Pane pane) {
         for (int i = 0; i < 24; i++) {
             Pane hour = new Pane();
@@ -89,23 +89,21 @@ public class OverviewDay extends Overview implements Initializable{
         }
     }
 
-    public void fillTimeContent(Pane pane, List<Activity> activities){
-        activities.stream().forEach(e -> System.out.println(e));
+    public void fillTimeContent(Pane pane, List<Activity> activities) {
         activities.stream().forEach(e -> showActivity(e));
 
 
     }
 
     //Test, Creating stardart ending dates for testpurposes
-    public Date standartEndDate(Date date){
+    public Date standartEndDate(Date date) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         cal.add(Calendar.HOUR_OF_DAY, 5);
         return cal.getTime();
     }
 
-    public void showActivity(Activity activity){
-        System.out.println("numbe2");
+    public void showActivity(Activity activity) {
         Calendar c = Calendar.getInstance();
         c.setTime(activity.getStartDate());
         int startHour = c.get(Calendar.HOUR_OF_DAY);
@@ -114,6 +112,7 @@ public class OverviewDay extends Overview implements Initializable{
         c.setTime(activity.getEndDate());
         int endHour = c.get(Calendar.HOUR_OF_DAY);
         int endMin = c.get(Calendar.MINUTE);
+        System.out.println(startHour +" " + startMin + " " +endHour + " " +endMin);
 
         Text description = new Text(activity.getDescription());
         description.getStyleClass().add("buttonContent");
@@ -127,37 +126,44 @@ public class OverviewDay extends Overview implements Initializable{
         Button eventButton = new Button("", buttonContent);
 
         //Calculating the position of the event, y relative to the height of the node
-        int yStart = (((startHour * 60) + startMin)*hourPanes.size() * 30)/1440;
-        int xStart = spaceUsed(activity) ? 200 : 60;
-        int yEnd = (((endHour * 60) + endMin)*hourPanes.size() * 30)/1440;
+        int yStart = (((startHour * 60) + startMin) * hourPanes.size() * 30) / 1440;
+        int yEnd = (((endHour * 60) + endMin) * hourPanes.size() * 30) / 1440;
+        int xStart = spaceUsed(yStart, yEnd) ? 120 : 60;
+
+        System.out.println(spaceUsed(yStart, yEnd));
+
+        System.out.println("yStart " + yStart + " yEnd: " + yEnd);
+
 
         eventButton.setLayoutX(xStart);
         eventButton.setLayoutY(yStart);
-        eventButton.setStyle("-fx-background-color: blue");
         //eventButton.getStyleClass().add("button");
 
-        eventButton.setPrefHeight(yEnd-yStart);
+        eventButton.setPrefHeight(yEnd - yStart);
         eventButton.setPrefWidth(60);
 
+        System.out.println(eventButton.getPrefHeight() + " " + eventButton.getLayoutY());
+        eventButtonList.add(eventButton);
         normalPane.getChildren().add(eventButton);
 
     }
 
     @Override
     protected void initialize() {
-        
+
     }
 
-    public boolean spaceUsed(Activity activity){
-        for (Activity act : activityList.getActivities()) {
-            if(activity.getStartDate().compareTo(act.getStartDate()) > 0 && activity.getEndDate().compareTo(act.getEndDate()) < 0){
+    public boolean spaceUsed(double yStart, double yEnd) {
+        for (Button b : eventButtonList) {
+            System.out.println(String.format("b1 start: %f, b1.end: %f, b2.start: %f, b2.end: %f", b.getLayoutY(), b.getLayoutY() + b.getPrefHeight(), yStart, yStart +  (yEnd - yStart)));
+            if ((yStart > b.getLayoutY() && yStart < b.getLayoutY() + b.getPrefHeight()) || yEnd > b.getLayoutY() && ) {
                 return true;
             }
         }
         return false;
     }
 
-    private DateFormatSymbols defineLocaleDate(){
+    private DateFormatSymbols defineLocaleDate() {
         Locale locale = new Locale("da", "DK");
         DateFormatSymbols dateFormatSymbols = new DateFormatSymbols(locale);
         dateFormatSymbols.setWeekdays(new String[]{
