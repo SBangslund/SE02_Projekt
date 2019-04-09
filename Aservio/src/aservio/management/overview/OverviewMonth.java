723e55f2961d32904e5fa8fffb64bea15588ac4d
Unknown <s.bangslund@hotmail.dk>
2019-04-09 09:42:07 +0200

    .SePlanlægning.OverviewMonth:

--- Aservio/src/aservio/management/overview/OverviewMonth.java
+++ Aservio/src/aservio/management/overview/OverviewMonth.java
@@ -9,10 +9,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.time.DayOfWeek;
-import java.time.Month;
-import java.time.MonthDay;
+import java.time.*;
 import java.util.*;
 
 public class OverviewMonth extends Overview implements Initializable, Pageable {
@@ -24,7 +21,8 @@ public class OverviewMonth extends Overview implements Initializable, Pageable {
     @FXML
     private GridPane gridPaneMonth;
 
-    private List<Pane> days = new ArrayList<>();
+    private List<Pane> daysObjects = new ArrayList<>();
+    private List<Date> daysDates = new ArrayList<>();
     private int currentMonth;
 
     public OverviewMonth() {
@@ -40,13 +38,12 @@ public class OverviewMonth extends Overview implements Initializable, Pageable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO create month overview
-        System.out.println("Created overview month");
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
                 VBox day = new VBox();
                 day.getChildren().add(new Label(j + ":" + i));
                 gridPaneMonth.add(day, j, i);
-                days.add(day);
+                daysObjects.add(day);
             }
         }
         populateDays(currentMonth);
@@ -55,26 +52,31 @@ public class OverviewMonth extends Overview implements Initializable, Pageable {
     public void populateDays(int month) {
         resetDays();
         GregorianCalendar calender = new GregorianCalendar();
-        GregorianCalendar monthCalender = new GregorianCalendar(calender.get(Calendar.YEAR), month - 1, 1);   // Creates new calender based on inserted month
-        calender.setTimeZone(TimeZone.getTimeZone("GMT+2"));                                                            // Corrects timezone to DK
+        GregorianCalendar monthCalender = new GregorianCalendar(calender.get(Calendar.YEAR), month, 1);   // Creates new calender based on inserted month
+        monthCalender.setTimeZone(TimeZone.getTimeZone("GMT+2"));                                                            // Corrects timezone to DK
         labelMonthTitle.setText(Month.of(month).toString().toLowerCase());
-        int firstDayOfWeek = monthCalender.get(Calendar.DAY_OF_WEEK) - 1;
-        int numberOfDaysInMonth = MonthDay.of(monthCalender.get(Calendar.MONTH), monthCalender.get(Calendar.DAY_OF_MONTH)).getMonth().length(monthCalender.isLeapYear(monthCalender.get(Calendar.YEAR))) - 1;
-        for (int i = firstDayOfWeek; i < numberOfDaysInMonth + firstDayOfWeek; i++) {
-            days.get(i - 1).getChildren().add(new Label(MonthDay.of(monthCalender.get(Calendar.MONTH), i - firstDayOfWeek + 1).toString()));
+        int firstDayOfWeek = new GregorianCalendar(calender.get(Calendar.YEAR), month - 1, 1).get(Calendar.DAY_OF_WEEK);
+        System.out.println(firstDayOfWeek);
+        int numberOfDaysInMonth = MonthDay.of(monthCalender.get(Calendar.MONTH), monthCalender.get(Calendar.DAY_OF_MONTH)).getMonth().length(monthCalender.isLeapYear(monthCalender.get(Calendar.YEAR)));
+        for (int i = firstDayOfWeek - 1; i < numberOfDaysInMonth + firstDayOfWeek - 1; i++) {
+            LocalDate locale = MonthDay.of(monthCalender.get(Calendar.MONTH), i - firstDayOfWeek + 2).atYear(monthCalender.get(Calendar.YEAR));
+            Date date = Date.from(locale.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+            daysDates.add(date);
+            daysObjects.get(i - 1).getChildren().add(new Label(date.toString()));
         }
     }
 
     public void resetDays() {
-        days.forEach(e -> {
+        daysObjects.forEach(e -> {
             e.getChildren().remove(1, e.getChildren().size());
         });
+        daysDates.clear();
     }
 
     @Override
     public void next() {
-        if (!days.isEmpty()) {
-            populateDays(Math.abs(++currentMonth % 12 + 1));
+        if (!daysObjects.isEmpty()) {
+            populateDays(Math.abs(++currentMonth % 12));
         } else {
             System.err.println("[WARNING][OverviewMonth] has yet to be initialized. populateDays() did not compute.");
         }
@@ -82,8 +84,8 @@ public class OverviewMonth extends Overview implements Initializable, Pageable {
 
     @Override
     public void previous() {
-        if (!days.isEmpty()) {
-            populateDays(Math.abs(--currentMonth % 12 + 1));
+        if (!daysObjects.isEmpty()) {
+            populateDays(Math.abs(--currentMonth % 12));
         } else {
             System.err.println("[WARNING][OverviewMonth] has yet to be initialized. populateDays() did not compute.");
         }
@@ -91,6 +93,10 @@ public class OverviewMonth extends Overview implements Initializable, Pageable {
 
     @Override
     public void showActivities(ActivityList activities) {
-        System.out.println(activities);
+        activities.getActivities().forEach(e -> {
+            if(daysDates.contains(e.getStartDate())) {
+                System.out.println(e);
+            }
+        });
     }
 }
