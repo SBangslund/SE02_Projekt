808d7c49208d327d47aabf8af86c1ea97e6c0b8f
Unknown <s.bangslund@hotmail.dk>
2019-04-16 14:44:28 +0200

    Fixed FXML path (works in netbeans and IntelliJ)

--- Aservio/src/aservio/management/overview/OverviewType.java
+++ Aservio/src/aservio/management/overview/OverviewType.java
@@ -1,9 +1,9 @@
 package aservio.management.overview;
 
 public enum OverviewType {
-    MONTH(0, "../views/FXMLOverviewMonth.fxml"),
-    WEEK(1, "../views/FXMLOverviewWeek.fxml"),
-    DAY(2, "../views/FXMLOverviewDay.fxml");
+    MONTH(0, "/aservio/management/views/FXMLOverviewMonth.fxml"),
+    WEEK(1, "/aservio/management/views/FXMLOverviewWeek.fxml"),
+    DAY(2, "/aservio/management/views/FXMLOverviewDay.fxml");
 
     private int type;
     private String url;
