899c9efc1b8608545f6769a2ab2149a4bdfed0ce
Bende Siewertsen <bende1308@gmail.com>
2019-05-08 09:41:37 +0200

    ActivityRetriever is implemented

--- Aservio/src/aservio/domain/platform/interfaces/contracts/IRepository.java
+++ Aservio/src/aservio/domain/platform/interfaces/contracts/IRepository.java
@@ -1,5 +1,6 @@
 package aservio.domain.platform.interfaces.contracts;
 
+import java.util.Date;
 import java.util.UUID;
 
 public interface IRepository {
@@ -14,4 +15,6 @@ public interface IRepository {
     boolean addUser(String username, String password, UUID userid);
     boolean addUserInfo(String mail, String firstname, String lastname, int phone, String picture, UUID userid, String institutionName);
     boolean addUserAddress(String roadname, String country, int postcode, String city, String housenumber, String level, UUID userid);
+    boolean addActivity(String name, String type, Date date, String starttime, String endtime, UUID activityid);
+    boolean addUserToActivity(UUID activityid, UUID userid);
 }
